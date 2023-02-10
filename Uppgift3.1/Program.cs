f02264eabd0b6dedd58e9551f3875d55737d110b
Anton <>
2023-02-10 08:32:34 +0100

    Add project files.

---
+++ Uppgift3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du måste vara mellan 16 och 19 år för att delta i tävlingen.\n" +
+                              "Hur gammal är du?");
+            int ålder = Convert.ToInt32(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 19)
+                Console.WriteLine("Du får delta i tävlingen.");
+
+            else if (ålder > 19)
+                Console.WriteLine("Du är för gammal");
+
+            else if (ålder < 16)
+                Console.WriteLine("Du är för ung");
+
+        }
+    }
+}
